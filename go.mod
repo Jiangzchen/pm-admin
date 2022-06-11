a06c070d58b46c0306e0a2da1c09aa6bca40e4ff
Jiangzchen <927764151@qq.com>
2022-06-11 11:00:30 +0800

    qq

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.15
 require github.com/astaxie/beego v1.12.1
 
 require (
+	github.com/llgcode/draw2d v0.0.0-20210904075650-80aa0a2a901d // indirect
 	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
 	github.com/smartystreets/goconvey v1.6.4
+	github.com/vcqr/captcha v0.0.0-20190226134436-33e818ba057b
 )
--- routers/router.go
+++ routers/router.go
@@ -2,7 +2,7 @@
  * @Author: Jiangzchen 927764151@qq.com
  * @Date: 2022-06-10 19:57:13
  * @LastEditors: Jiangzchen 927764151@qq.com
- * @LastEditTime: 2022-06-10 20:21:55
+ * @LastEditTime: 2022-06-11 10:57:18
  * @FilePath: \pm-admin\routers\router.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,7 +19,7 @@ import (
 func init() {
 	beego.Router("/", &controllers.MainController{})
 	// beego.Router("/index", &controllers.SystemController{}, "get:Index")
-	beego.Router("/login", &system.SystemController{}, "get,post:Login")
-	// beego.Router("/captcha", &controllers.SystemController{}, "get:Captcha")
+	beego.Router("/system/login", &system.SystemController{}, "get,post:Login")
+	beego.Router("/system/captcha", &system.SystemController{}, "get:Captcha")
 
 }
