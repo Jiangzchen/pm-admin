92366357034d65d96f8abeb89d2e23707d221b47
Jiangzchen <927764151@qq.com>
2022-06-11 11:35:25 +0800

    添加了用户和角色、菜单模块

--- controllers/modules/system/system_controller.go
+++ controllers/modules/system/system_controller.go
@@ -2,7 +2,7 @@
  * @Author: Jiangzchen 927764151@qq.com
  * @Date: 2022-06-10 20:18:57
  * @LastEditors: Jiangzchen 927764151@qq.com
- * @LastEditTime: 2022-06-10 20:23:29
+ * @LastEditTime: 2022-06-11 11:09:08
  * @FilePath: \pm-admin\controllers\system.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,10 +21,13 @@ type SystemController struct {
 }
 
 func (this *SystemController) Login() {
-
 	// 渲染模板
 	this.TplName = "backend/system/login.tpl"
+}
 
+func (this *SystemController) Index() {
+	// 渲染模板
+	this.TplName = "backend/system/index.tpl"
 }
 
 func (this *SystemController) Captcha() {
--- routers/router.go
+++ routers/router.go
@@ -2,7 +2,7 @@
  * @Author: Jiangzchen 927764151@qq.com
  * @Date: 2022-06-10 19:57:13
  * @LastEditors: Jiangzchen 927764151@qq.com
- * @LastEditTime: 2022-06-11 10:57:18
+ * @LastEditTime: 2022-06-11 11:33:29
  * @FilePath: \pm-admin\routers\router.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,8 +18,17 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
-	// beego.Router("/index", &controllers.SystemController{}, "get:Index")
+	beego.Router("/system/index", &system.SystemController{}, "get:Index")
 	beego.Router("/system/login", &system.SystemController{}, "get,post:Login")
 	beego.Router("/system/captcha", &system.SystemController{}, "get:Captcha")
+
+	// 用户管理
+	beego.Router("/user/view", &system.UserController{}, "get:View")
+
+	// 角色管理
+	beego.Router("/role/view", &system.RoleController{}, "get:View")
+
+	// 菜单管理
+	beego.Router("/menu/view", &system.MenuController{}, "get:View")
 
 }
