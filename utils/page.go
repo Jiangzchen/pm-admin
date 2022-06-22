638617b77325c3def04a4f727d76c5c87a10b5f3
Jiangzchen <927764151@qq.com>
2022-06-22 21:38:36 +0800

    22提交

---
+++ utils/page.go
@@ -0,0 +1,27 @@
+/*
+ * @Author: Jiangzchen 927764151@qq.com
+ * @Date: 2022-06-22 21:27:04
+ * @LastEditors: Jiangzchen 927764151@qq.com
+ * @LastEditTime: 2022-06-22 21:27:33
+ * @FilePath: \pm-admin\utils\page.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package utils
+
+type Page struct {
+	PageNo     int
+	PageSize   int
+	TotalPage  int
+	TotalCount int
+	FirstPage  bool
+	LastPage   bool
+	List       interface{}
+}
+
+func PageHelper(count int, pageNo int, pageSize int, list interface{}) Page {
+	tp := count / pageSize
+	if count%pageSize > 0 {
+		tp = count/pageSize + 1
+	}
+	return Page{PageNo: pageNo, PageSize: pageSize, TotalPage: tp, TotalCount: count, FirstPage: pageNo == 1, LastPage: pageNo == tp, List: list}
+}
